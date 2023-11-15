005014808fef209f8c45e00f297d91ad60f3ad2d
Vanessa Cheong <i2sign@Vanessaui-MacBookPro.local>
2023-11-15 13:53:17 +0900

    instagram

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'style.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -21,6 +26,32 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
+  var data = [];
+  var userImage;
+
+  saveData() async {
+    var storage = await SharedPreferences.getInstance();
+    storage.setString('이름', '데이터');
+    var result = storage.get('이름');
+    print(result);
+  }
+
+  getData() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    if (result.statusCode == 200) {
+      print('성공');
+    } else {
+      print('실패');
+    }
+    setState(() => data = jsonDecode(result.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +61,25 @@ class _MyAppState extends State<MyApp> {
         actions: [
           IconButton(
             icon: Icon(Icons.add_box_outlined),
-            onPressed: () {},
+            onPressed: () async {
+              var picker = ImagePicker();
+              var image = await picker.pickImage(source: ImageSource.gallery);
+              if (image != null) {
+                setState(() {
+                  userImage = File(image.path);
+                });
+              }
+
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (c) => Upload(userImage: userImage)));
+            },
             iconSize: 30,
           )
         ],
       ),
-      body: [MyPost(), Text('샵페이지')][tab],
+      body: [MyPost(data: data), Text('샵페이지')][tab],
       bottomNavigationBar: BottomNavigationBar(
           showSelectedLabels: false,
           showUnselectedLabels: false,
@@ -54,29 +98,99 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class MyPost extends StatelessWidget {
-  const MyPost({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 3,
-        itemBuilder: (c, i) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset('assets/bus.jpg'),
-              Text(
-                "좋아요 100",
-              ),
-              Text(
-                "글쓴이 ",
-              ),
-              Text(
-                "글내용",
-              )
-            ],
-          );
-        });
+    return Container();
+  }
+}
+
+class MyPost extends StatefulWidget {
+  const MyPost({this.data, super.key});
+  final data;
+
+  @override
+  State<MyPost> createState() => _MyPostState();
+}
+
+class _MyPostState extends State<MyPost> {
+  var scroll = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scroll.addListener(() {
+      print(scroll.position.maxScrollExtent);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(
+          itemCount: 3,
+          controller: scroll,
+          itemBuilder: (c, i) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  child: Text(
+                    widget.data[i]['user'],
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (c) => Profile()));
+                  },
+                ),
+                Image.network(widget.data[i]['image']),
+                Text(
+                  widget.data[i]['content'],
+                )
+              ],
+            );
+          });
+    } else {
+      return CircularProgressIndicator();
+    }
+  }
+}
+
+class Upload extends StatelessWidget {
+  const Upload({this.userImage, super.key});
+  final userImage;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.file(userImage),
+            Text('이미지 업로드 화면'),
+            TextField(),
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ));
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('프로필 페이지'),
+    );
   }
 }
